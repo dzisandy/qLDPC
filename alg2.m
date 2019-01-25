be87c9963eb8b8c385253d4eb0a9a72b33ae3451
Andrei Dzis <dzisandy@mail.ru>
2019-01-25 19:53:22 +0300

    added alg2

---
+++ alg2.m
@@ -0,0 +1,14 @@
+function [c,F] = alg2(r,m, H, thetas)
+[~,t] = size(thetas); 
+H_gf = gf(full(H.'),m);
+S = gf(r,m)*H_gf;
+for i = 1:t-1
+    [~,r] = alg1(r, m, H, thetas(i))
+end
+F = 1;
+c = r;
+if nnz(S.x) == 0
+    F = 0;
+end
+
+end
--- test.m
+++ test.m
@@ -1,7 +1,7 @@
 [H,Q] = alist2sparse('test.alist') %parity check matrix in sparse format
 [M,N] = size(H); % defining sizes
 m  = 9; % gf(2^m)
-p = 0.05; %error probability
+p = 0.01; %error probability
 r = noise_cdw(N,p,m)%received sequince
 theta = 0; %threshold
 alg1(r, m, H, theta) % algorithm 1
