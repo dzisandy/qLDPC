80d3a2de9116b320e2a9b09300e6493f7446459c
Andrei Dzis <dzisandy@mail.ru>
2019-01-29 13:31:24 +0300

    added results

--- alg2.m
+++ alg2.m
@@ -2,8 +2,8 @@ function [c,F] = alg2(r,m, H, thetas)
 [~,t] = size(thetas); 
 H_gf = gf(full(H.'),m);
 S = gf(r,m)*H_gf;
-for i = 1:t-1
-    [~,r] = alg1(r, m, H, thetas(i))
+for i = 1:t
+    [r,~] = alg1(r, m, H, thetas(i));
 end
 F = 1;
 c = r;
--- test_alg2.m
+++ test_alg2.m
@@ -1,17 +1,19 @@
 [H,Q] = alist2sparse('test.alist'); %parity check matrix in sparse format
 [M,N] = size(H); % defining sizes
 m  = 9; % gf(2^m)
-fid = fopen('FER.txt', 'w');
-%p = 0.02; %error probability
-%r = noise_cdw(N,p,m); %received sequince
-theta = 0; %threshold
+fid = fopen('FER_2.txt', 'w');
+p = 0.03;
+%H = full(H);
+%r = noise_cdw(N,p,m); 
+thetas = 1:54
+%alg2(r, m, H, thetas)
 for j = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20]
     fer = 0;
     er = 0;
     for i = 1:500
         disp(i)
         r = noise_cdw(N,j,m);
-        [c,~] = alg1(r, m, H, theta);
+        [c,~] = alg2(r, m, H, thetas);
         if c == zeros(1, N)
             er = er + 1;
         end
@@ -20,6 +22,11 @@ for j = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 
     fer = (500-er)/500
     fprintf(fid,'%d\n', fer); 
 end
-        
-        
-    
+%max weight of H row is 54 
+%
+% proof
+%
+%for i = 1:N
+%    z(i) = sum(H(:,i) )
+%end
+%max(z)
