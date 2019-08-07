2eaef69b328ff1e1a71edeb211939b830ba7baf2
Andrei Dzis <dzisandy@mail.ru>
2019-08-07 17:42:32 +0300

    updated milestone 1

---
+++ MatLab/alg1.m
@@ -0,0 +1,52 @@
+function [c,F] = alg1(r, m, H, theta)
+[~,N] = size(H);
+H_gf = gf(full(H.'),m);
+S = gf(r,m)*H_gf;
+b = 1;
+itr = 0;
+while b == 1
+    b = 0;
+    for i = 1:N
+        j = find(H(:,i));
+        msg = gf(full(H(j,i)),m).^-1;
+        k = msg.' .* S(j);
+        k = k.x;
+        [~,l] = size(k);
+        z = l - nnz(k);
+        y = zeros(size(k));
+        for p = 1:length(k)
+            if k(p) ~= 0
+                y(p) = sum(k==k(p));
+            else 
+                y(p) = 0;
+            end
+        end
+        [a, argmax] = max(y);
+        M = k(argmax);
+        if a - z >= theta
+            final = gf(r(i),m) + gf(M, m);
+            r(i) = final.x;
+            S = gf(r,m)*H_gf;
+            b = 1;
+        end
+    end
+    itr = itr + 1;
+    if itr >= 100
+        c = r;
+        F = 1;
+        break
+    end
+    F = 1;
+    c = r;
+    if nnz(S.x) == 0
+        F = 0;
+    end
+    
+
+      
+    end
+
+            
+            
+    
+end
